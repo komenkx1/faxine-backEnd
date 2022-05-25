01a8c5fdf5cac10c737fa1085d5d46d892b7b203
Andrian17 <andriancimen123@gmail.com>
2022-05-25 23:26:20 +0800

    membuat migration DB dan data dummy

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'no_hp',
     ];
 
     /**
@@ -41,4 +42,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function news()
+    {
+        return $this->hasMany(News::class);
+    }
 }
--- database/migrations/2022_05_25_221817_create_news_table.php
+++ database/migrations/2022_05_25_221817_create_news_table.php
@@ -13,14 +13,13 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('news', function (Blueprint $table) {
             $table->id();
-            $table->string('username');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('no_hp');
-            $table->rememberToken();
+            $table->string('user_id');
+            $table->string('judul');
+            $table->string('slug');
+            $table->text('content');
+            $table->string('cover');
             $table->timestamps();
         });
     }
@@ -32,6 +31,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('news');
     }
 };
